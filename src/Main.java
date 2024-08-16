7d04536298f72a58c48aa6e30a3202fae221d5dd
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-16 08:29:59 -0400

    mnj

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if(hourOfDay < 0 || hourOfDay > 23){
+            return false;
+        }
+        else if (barking && (hourOfDay < 8 || hourOfDay > 22)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
